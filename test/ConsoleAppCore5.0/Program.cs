e7a45eb698f74c261c27a06415279d8bf7171591
AllenXu <734329218@qq.com>
2021-02-09 10:59:53 +0800

    升级nuget包

---
+++ test/ConsoleAppCore5.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCore5._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
